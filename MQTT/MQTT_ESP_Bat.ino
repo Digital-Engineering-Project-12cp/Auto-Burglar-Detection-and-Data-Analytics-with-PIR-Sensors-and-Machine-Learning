9f1fdece628ca8e9c77b9799a2be7d75c57f6ea9
manish978973 <manishrchandran@gmail.com>
2019-10-17 02:32:53 +0200

    Update MQTT_ESP_Bat.ino
    Earlier uploaded was a mismatched one

--- MQTT/MQTT_ESP_Bat.ino
+++ MQTT/MQTT_ESP_Bat.ino
@@ -1,108 +1,37 @@
-/*
- Basic ESP8266 MQTT PIR sketch
-
-*/
-
 #include <ESP8266WiFi.h>
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"
+#include <PubSubClient.h>
+
+
 
 // Update these with values suitable for your network.
 
-/********* WiFi Access Point ***********/
+const char* ssid = "nowire";
+const char* password = "dontaskmeagain";
+const char* mqtt_server = "192.168.0.106";
+const char* pub_topic = "/feeds/motion";
 
-#define WLAN_SSID       "******"           // Wi-Fi network name
-#define WLAN_PASS       "*******"           // Wi-Fi password
+//Static IP address configuration
+IPAddress staticIP(192, 168, 0, 109); //ESP static ip
+IPAddress gateway(192, 168, 0, 1);   //IP Address of your WiFi Router (Gateway)
+IPAddress subnet(255, 255, 255, 0);  //Subnet mask
+IPAddress dns(0, 0, 0, 0);  //DNS
 
-/********** MQTT Broker ************/
-
-#define AIO_SERVER      "********"  // MQTT broker IP
-#define AIO_SERVERPORT  1883             // MQTT broker port
-#define AIO_USERNAME    ""           // MQTT username
-#define AIO_KEY         ""           // MQTT password
-#define AIO_CID         ""     // MQTT client ID
-
-
-IPAddress staticIP(192, 168, 0, 109);
-// Set your Gateway IP address
-IPAddress gateway(192, 168, 0, 1);
-
-IPAddress subnet(255, 255, 255, 0);
-
-
-
-
-
-
-// Start a counter for serial logging and set the initial value to no motion
-int counter = 0;
-int previousReading = LOW;
-
-WiFiClient client;
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY, AIO_CID);
-
-// Setup publish feeds - define topic name in parenthesis
-Adafruit_MQTT_Publish status  = Adafruit_MQTT_Publish(&mqtt, AIO_CID "/feeds/motion");
-Adafruit_MQTT_Publish motion_topic  = Adafruit_MQTT_Publish(&mqtt, AIO_CID "/feeds/motion");
-
+WiFiClient espClient;
+PubSubClient client(espClient);
 long lastMsg = 0;
 char msg[50];
 int value = 0;
 
-/////////////////////////////
-//VARS
-//the time we give the sensor to calibrate (10-60 secs according to the datasheet)
-int calibrationTime = 0;
 
-//the time when the sensor outputs a low impulse
-long unsigned int lowIn;
+char message_buff[50];
+char message_buff1[50];
 
-//the amount of milliseconds the sensor has to be low
-//before we assume all motion has stopped
-long unsigned int pause = 5000;
-
-boolean lockLow = true;
-boolean takeLowTime;
-
-int pirPin = 4;    // the digital pin connected to the PIR sensor's output
+int pirPin = 4;
 int ledPin = 2;    // the digital pin connected to built-in LED
 int switchPin = 5;
 bool switchStatus = false;
-
-int sensorValue = 0;  //ADC battery level in range 0 -1024
-
-const int analogInPin = A0;  //for battery level
-
-void MQTT_connect();
-
-void setup_wifi() {
-
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(WLAN_SSID);
-  WiFi.config(staticIP, gateway, subnet);
-
-  WiFi.begin(WLAN_SSID, WLAN_PASS);
-
-
-
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
+int sensorValue = 0;
+const int analogInPin = A0;
 
 float battery_level(){
 
@@ -114,92 +43,137 @@ float batteryLevel =  sensorValue  * caliberationValue;
 return batteryLevel;
 }
 
-  // Setup a MQTT subscription
 
 
-void MQTT_connect() {
-  int8_t ret;
+void setup_wifi() {
 
-  // Stop if already connected.
-  if (mqtt.connected()) {
-    return;
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.config(staticIP, subnet, gateway);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(20);
+    Serial.print(".");
   }
 
+  randomSeed(micros());
 
-
-
-  Serial.print("Connecting to MQTT... ");
-
-  uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-       Serial.println(mqtt.connectErrorString(ret));
-       Serial.println("Retrying MQTT connection in 5 seconds...");
-       mqtt.disconnect();
-       delay(5000);  // wait 5 seconds
-       retries--;
-       if (retries == 0) {
-         // basically die and wait for WDT to reset me
-         while (1);
-       }
-  }
-  Serial.println("MQTT Connected!");
- // status.publish("online");
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
 void setup() {
-  //pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+
   Serial.begin(115200);
   setup_wifi();
-  pinMode(pirPin, INPUT);
+   pinMode(pirPin, INPUT);
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, HIGH);
   digitalWrite(pirPin, LOW);
   pinMode(switchPin, INPUT_PULLUP);
   pinMode(0, OUTPUT);
   digitalWrite(0, LOW);
+  
+  client.setServer(mqtt_server, 1883);
+  //client.setCallback(callback);
 
+    if (!client.connected()) {
+    reconnect();
+  }
+  // client.publish(pub_topic, "motion/home");
 
-MQTT_connect();
-
-       if(digitalRead(switchPin) == LOW){
+ if(digitalRead(switchPin) == LOW){
 
           float batval = battery_level();
-         // motion_topic.publish("Motion/Home" + String(batval));
-          motion_topic.publish(String(batval));
+          String pubString = "MotionHome/" + String(batval);
+          pubString.toCharArray(message_buff, pubString.length()+1);
+          client.publish(pub_topic,message_buff);
+ 
           Serial.println("---");
-        //  Serial.println("Motion/Home" + String(batval));
-          Serial.println(String(batval));
+          Serial.println("MotionHome/" + String(batval));
+          
 
        }
        else {
         if(digitalRead(switchPin) == HIGH){
           float batval1 = battery_level();
-         // motion_topic.publish("Motion/Away" + String(batval1));
-         motion_topic.publish(String(batval1));
+          String pubString1 = "MotionAway/" + String(batval1);
+          pubString1.toCharArray(message_buff1, pubString1.length()+1);
+          
+          client.publish(pub_topic,message_buff1);
+        
          Serial.println("---");
-        // Serial.println("Motion/Away" + String(batval1));
-        Serial.println(String(batval1));
+         Serial.println("MotionAway/" + String(batval1));
+       
         }
 
 
        }
 
-       delay(2000);
-        Serial.println("Entering Deep Sleep");
-       ESP.deepSleep(0);
 
-  //give the sensor some time to calibrate
+  delay(2000);
+  Serial.println("\ngoing to sleep");
+  delay(100);
+  ESP.deepSleep(0);
+  delay(100);
+  Serial.println("\nsleep unsuccessful");
 
-
-
+ 
 }
 
 void loop() {
-  // Ensure the connection to the MQTT server is alive (this will make the first
-  // connection and automatically reconnect when disconnected).  See the MQTT_connect
-  // function definition further below.
-
-
 
 
 }
